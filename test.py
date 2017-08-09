6c4354be68edb173252a2f2b3923492fba0024f4
lukelin <dev.linxu@gmail.com>
2017-08-09 17:00:50 -0400

    UI improve

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ def sel(var1):
    im = cv2.imread('3.jpg')
    im = cv2.GaussianBlur(im, (3, 3), 0)
    canny = cv2.Canny(im, int(var1), int(var1)*3)
+   # output1 = []
+   # output2 = []
+   # cv2.findContours(canny, output1, output2, )
    cv2.imwrite(path, canny)
    img2 = ImageTk.PhotoImage(PIL.Image.open(path))
    panel.configure(image=img2)
@@ -17,12 +20,12 @@ def sel(var1):
 root = Tk()
 var = DoubleVar()
 scale = Scale( root, variable = var, command=sel)
-scale.pack(anchor=CENTER)
+scale.pack(anchor=CENTER, side = "left")
 
 path = 'canny.jpg'
 
 img = ImageTk.PhotoImage(PIL.Image.open(path))
 panel = Label(root, image = img)
-panel.pack(side = "bottom", fill = "both", expand = "yes")
+panel.pack(side = "right", fill = "both", expand = "yes")
 root.mainloop()
 
