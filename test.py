01f86188c73e87728265b0a140a22af85d249bdf
LukeLin <dev.linxu@gmail.com>
2017-08-15 23:10:47 -0400

    good to 1.jpg and 2.jpg

--- test.py
+++ test.py
@@ -6,12 +6,16 @@ from tkinter import *
 
 
 def sel(var1):
-   var1 = '20'
-   im = cv2.imread('2.jpg')
-   im = cv2.GaussianBlur(im, (3, 3), 0)
+   # var1 = '20'
+   im = cv2.imread('3.jpg')
+   # im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+   # im = cv2.pyrMeanShiftFiltering(im, 25, 10)
    canny = cv2.Canny(im, int(var1), int(var1)*3)
-   ret, thresh = cv2.threshold(canny, 127, 255, 0)
-   im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+   # lines = cv2.HoughLines(canny, 1, 3.14 / 180, 200)
+   # ret, thresh = cv2.threshold(canny, 127, 255, 0)
+   kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+   closed = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+   im2, contours, hierarchy = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
    area = -1
    cnt = contours[0]
@@ -21,12 +25,14 @@ def sel(var1):
            cnt = cnt0
            area = t
 
-   # epsilon = 0.1 * cv2.arcLength(cnt, True)
-   # approx = cv2.approxPolyDP(cnt, epsilon, True)
-   # cv2.drawContours(im, [approx], -1, (0, 255, 0), 3)
+   epsilon = 0.1 * cv2.arcLength(cnt, True)
+   approx = cv2.approxPolyDP(cnt, epsilon, True)
+   cv2.drawContours(im, [approx], -1, (0, 255, 0), 3)
+   cv2.drawContours(im, [cnt], -1, (0, 0, 255), 3)
 
-   cv2.drawContours(im, contours, -1, (0, 255, 0), 3)
-   cv2.imwrite(path, im2)
+   # cv2.drawContours(im, contours, -1, (0, 255, 0), 3)
+   # cv2.line(im, lines, (0,0,255),2)
+   cv2.imwrite(path, im)
    img2 = ImageTk.PhotoImage(PIL.Image.open(path))
    panel.configure(image=img2)
    panel.image = img2
